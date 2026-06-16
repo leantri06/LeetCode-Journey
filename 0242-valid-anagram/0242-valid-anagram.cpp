7b4bb1ec3a7f60cbf36db2fc01e3691bead1ca62
Le An Tri <star.leantrii.1909@gmail.com>
2026-06-16 23:42:52 +0700

    Time: 0 ms (100%), Space: 9.7 MB (75.65%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char, int> hash;
         if (s.size() != t.size()) return false;
+        int count[26] = {0};
         for (int i = 0; i < s.size(); i++) {
-            hash[s[i]]++;
-            hash[t[i]]--;
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
         }
-        for (int i = 0; i < s.size(); i++) {
-            if (hash[s[i]] != 0) return false;
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
         }
         return true;
     }
