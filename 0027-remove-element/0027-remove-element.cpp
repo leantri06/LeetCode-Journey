401bf341ff23436a314a7ce67f214ba8129d60b1
Le An Tri <star.leantrii.1909@gmail.com>
2026-06-16 13:06:45 +0700

    Time: 0 ms (100%), Space: 11.7 MB (47.42%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        for (int i = 0; i < nums.size();) {
-            if (nums[i] == val) {
-                nums.erase(nums.begin() + i);
-            } else {
-                i++;
+        int index = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
             }
         }
-        return nums.size();
+        return index;
     }
 };
