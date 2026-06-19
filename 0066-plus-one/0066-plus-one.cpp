16ba070d727150122cef69902b1ddfe266f9dfc7
Le An Tri <star.leantrii.1909@gmail.com>
2026-06-20 00:14:45 +0700

    Time: 0 ms (100%), Space: 11.7 MB (26.58%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i] += 1;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        reverse(digits.begin(), digits.end());
+        digits.push_back(1);
+        reverse(digits.begin(), digits.end());
+        return digits;
+    }
+};
