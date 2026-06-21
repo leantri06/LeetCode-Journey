69b020bdb7936830c681437adf061b67dc13a678
Le An Tri <star.leantrii.1909@gmail.com>
2026-06-22 01:55:08 +0700

    Time: 10 ms (86.61%), Space: 36.3 MB (93.61%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
+        if (nums.size()==1) return 0;
         sort(nums.begin(), nums.end());
         int l = 0;
         int max_length = 0;
