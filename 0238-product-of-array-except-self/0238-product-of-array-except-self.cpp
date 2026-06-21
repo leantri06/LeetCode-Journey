1d37d8d134ec3861173a2c7ee36020f7c89ecab1
Le An Tri <star.leantrii.1909@gmail.com>
2026-06-21 22:42:50 +0700

    Time: 0 ms (100%), Space: 40.2 MB (80.64%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,16 +4,14 @@ public:
         int prefix = 1;
         vector<int> dp1(nums.size(), 1);
         int suffix = 1;
-        vector<int> dp2(nums.size(), 1);
         for (int i = 0; i < nums.size(); i++) {
             dp1[i] = prefix;
             prefix *= nums[i];
-            dp2[nums.size()-1-i] = suffix;
-            suffix *= nums[nums.size()-1-i];
         }
-        for (int i = 0; i < nums.size(); i++) {
-            nums[i] = dp1[i]*dp2[i];
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            dp1[i] *= suffix;
+            suffix *= nums[i];
         }
-        return nums;
+        return dp1;
     }
 };
