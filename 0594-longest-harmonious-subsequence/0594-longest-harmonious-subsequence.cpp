faa39e4d1ead5c5465854daf6359dbbb9afd12c3
Le An Tri <star.leantrii.1909@gmail.com>
2026-06-22 01:48:24 +0700

    Time: 8 ms (91.01%), Space: 36.3 MB (84.09%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
-        int max_freq = 0;
-        unordered_map<int, int> hash;
-        for (int x : nums) {
-            hash[x]++;
+        sort(nums.begin(), nums.end());
+        int l = 0;
+        int max_length = 0;
+        for (int r = 0; r < nums.size(); r++) {
+            if (nums[r] - nums[l] == 1) {
+                max_length = max(max_length, r-l+1);
+            } 
+            while (nums[r] - nums[l] > 1) {
+                l++;
+            }
         }
-        for (int i = 0; i < nums.size(); i++) {
-            if (hash[nums[i]+1] > 0) max_freq = max(max_freq, hash[nums[i]] + hash[nums[i]+1]);
-        }
-        return max_freq;
-    }
+        return max_length;
+    } 
 };
