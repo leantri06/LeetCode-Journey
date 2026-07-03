f22f1b6121ccff236569e6f1a3f30e9f2f845242
Le An Tri <star.leantrii.1909@gmail.com>
2026-07-03 22:17:09 +0700

    Time: 0 ms (100%), Space: 11.5 MB (20.92%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int res = 0; 
+        for (int i = 0; i < accounts.size(); i++) {
+            int money = 0;
+            for (int j = 0; j < accounts[0].size(); j++) {
+                money += accounts[i][j];
+            }
+            res = max(res, money);
+        }
+        return res;
+    }
+};
