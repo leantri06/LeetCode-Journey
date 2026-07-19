d043007cf7117fe374839cd83e8ee1468078fe30
Le An Tri <star.leantrii.1909@gmail.com>
2026-07-20 05:28:53 +0700

    Time: 3 ms (34.46%), Space: 63.7 MB (11.15%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int l = 0;
         int r = height.size() - 1;
         int res = 0;
