cc3cfd98d16abe4479d0df7b504b4343af99d614
Le An Tri <star.leantrii.1909@gmail.com>
2026-08-01 23:55:20 +0700

    Time: 2 ms (30.9%), Space: 9.5 MB (7.76%) - LeetHub

--- 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.cpp
+++ 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int countKConstraintSubstrings(string s, int k) {
         int res = 0;
-        for (int i = 0; i < s.size(); i++) {
-            unordered_map<char, int> hash;
-            for (int j = i; j < s.size(); j++) {
-                hash[s[j]]++;
-                if (hash['0'] <= k || hash['1'] <= k) {
-                    res++;
-                }
-            }
+        int left = 0;
+        unordered_map<char, int> hash;
+        for (int right = 0; right < s.size(); right++) {
+            hash[s[right]]++;
+            while (hash['0'] > k && hash['1'] > k) {
+                hash[s[left]]--;
+                left++;
+            } 
+            res += (right - left + 1);
         }
         return res;
     }
